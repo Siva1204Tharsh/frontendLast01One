4fd69ea0cea8806ec061387ba6fad92681892dd1
Siva1204Tharsh <sivasivapatham12@gmail.com>
2024-06-15 17:29:41 +0530

    j

--- src/pages/Tour/HiddenPlaces.jsx
+++ src/pages/Tour/HiddenPlaces.jsx
@@ -30,16 +30,7 @@ const HiddenPlaces = () => {
     //         src="https://firebasestorage.googleapis.com/v0/b/travely-7264c.appspot.com/o/dsc_0128.webp?alt=media&token=a06ac6b6-450a-49ac-9330-e345e9e0e755"
     //         alt=""
     //       />
-    //     </div>
-    //     <div className="text-xl mt-5">
-    //       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt fugiat
-    //       cupiditate quas vitae, ducimus saepe quos voluptatibus, accusantium
-    //       qui voluptates earum voluptatum. Consequuntur alias beatae laborum
-    //       neque quisquam, quae sit, inventore maxime sed eligendi aliquid a cum
-    //       voluptatum nostrum deleniti cumque minima ab ullam impedit eveniet
-    //       nulla sequi quis odit?
-    //     </div>
-    //   </div>
+    //   
     // </div>
     <div class="mx-auto max-w-2xl px-4 pb-16 sm:px-6 lg:max-w-7xl lg:px-8 mt-10">
       <p class="uppercase text-5xl">
