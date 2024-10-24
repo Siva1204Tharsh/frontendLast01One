d324d737de413a5aca36deaa4849cd3329064d39
Siva1204Tharsh <sivasivapatham12@gmail.com>
2024-10-24 09:25:17 +0530

    u

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ export default function App() {
   return (
     <BrowserRouter>
       <Layout />
+      
     </BrowserRouter>
+    
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,6 +16,7 @@ const Home = () => {
       <Hero3 />
       <Aboutus />
       <Services />
+
     </div>
   );
 };
